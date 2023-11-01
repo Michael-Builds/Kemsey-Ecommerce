59aa2c9b4f96c5d2ca7b0f767fbc7e9fcd761a8b
Kabanda Kpanti Michael <michaelkpantiramp@gmail.com>
2023-11-01 22:27:18 +0000

    Shop Module Underway

--- src/pages/shop/component/Main.jsx
+++ src/pages/shop/component/Main.jsx
@@ -273,15 +273,15 @@ const Main = () => {
                     {products.slice(0, visibleProducts).map((product, index) => (
                         <div key={index} className="bg-white shadow rounded overflow-hidden group shadow-lg">
                             <div className="relative">
-                                <img src={product.imgSrc} alt={`Product ${index + 1}`} className="w-full" />
-                                <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
+                                <img src={product.imgSrc} alt={`Product ${index + 1}`} className="w-full cursor-pointer" />
+                                {/* <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
                                     <a href="#" className="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition" title="view product">
                                         <BsSearch />
                                     </a>
                                     <a href="#" className="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition" title="add to wishlist">
                                         <BsHeartFill />
                                     </a>
-                                </div>
+                                </div> */}
                             </div>
                             <div className="pt-4 pb-3 px-4">
                                 <a href="#">
